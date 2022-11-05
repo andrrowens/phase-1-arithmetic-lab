6f080882df01c621a75f891186d322b5bd1b08d7
Andrew Owens <andrrowens@gmail.com>
2022-11-05 16:54:48 -0600

    commit tree c0855de90c1230c63bf9b6f93228abded25b91e5

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+
+const num1 = 2
+const num2 = 31
+
+const multiply = num1 * num2
+
+multiply
+
+const random = Math.ceil(Math.random())
+
+const num3 = 14
+const num4 = 5
+
+const mod = num3 % num4
+
+const max = Math.max(5, 10, 15, 20);
